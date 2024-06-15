6147c676683b11c6327fdf8b16ad2d12b8a9440b
rakeshcheekatimala <rakeshcheekatimala@gmail.com>
2024-06-15 18:58:11 +0800

    :feat: how to create chatgpt integration with context embedded.

---
+++ chat-app/src/basic_chat/index.ts
@@ -0,0 +1,36 @@
+import { OpenAI } from 'openai'
+
+const openai = new OpenAI()
+
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  {
+    role: 'system',
+    content: 'You are a helpful chatbot',
+  },
+]
+
+async function createChatCompletion() {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: context,
+  })
+  const responseMessage = response.choices[0].message
+
+  context.push({
+    role: 'assistant',
+    content: responseMessage.content,
+  })
+  console.log(
+    `${response.choices[0].message.role} : ${response.choices[0].message.content}`
+  )
+}
+
+process.stdin.addListener('data', async function (input) {
+  const userInput = input.toString().trim()
+
+  context.push({
+    role: 'user',
+    content: userInput,
+  })
+  await createChatCompletion()
+})
